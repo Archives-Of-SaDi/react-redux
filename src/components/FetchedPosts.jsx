ca49f802fb776844fcd9bf65d6f77dd1c122e902
sad1go0 <sayidullohabbasov0909@gmail.com>
2022-09-30 22:19:15 +0500

    edited

---
+++ src/components/FetchedPosts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FetchedPosts() {
+  return (
+    <>
+      <h1>Fetched Posts</h1>
+    </>
+  );
+}
+
+export default FetchedPosts;
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Post Form</h1>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
